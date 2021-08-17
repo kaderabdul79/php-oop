c6254079480eaa6fa1d805ce8490cfd7bf11ef1e
kaderabdul22 <mostafakamal9300@gmail.com>
2021-08-17 18:49:40 +0600

    creating class,obj and method

--- index.php
+++ index.php
@@ -1,28 +1,9 @@
 <?php
 
 class BankAccount{
-    public $accountNo;
-    public $balance;
-
-    public function deposit($amount){
-        if($amount > 0){
-            $this->balance += $amount;
-            //return $this->balance;
-        }
-    }
-
-    // public function withdraw($amount){
-    //     if($amount <= $this->balance){
-    //         $this->balance -= $amount;
-    //         return true;
-    //     }
-    //     return false;
-    // }
+   public function display(){
+       echo "learning php OOP";
+   }
 }
-//$account = new BankAccount();
 $account = new BankAccount;
-$account->accountNo = 1;
-$account->balance = 200;
-
-$account->deposit(300);
-//print_r($account);
+$account->display();
