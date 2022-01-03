b4755aa40207cc04c60bec4c4994a168146b3685
kaderabdul <kaderabdul7980@gmail.com>
2022-01-03 18:41:53 +0600

    apply json_decode on json data and converted fucn array to string and apply loop

--- JSON/index.php
+++ JSON/index.php
@@ -1,2 +1,8 @@
 <?php
-echo file_get_contents('drinks.json');
+$drinks = file_get_contents('drinks.json');
+$drinks = json_decode($drinks);
+// echo $drinks;
+// print_r($drinks);
+foreach($drinks as $drink){
+     echo '<li>'.$drink->name.' and price is $'.$drink->price.'</li>';
+}
